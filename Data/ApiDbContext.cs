38913051fa69a801a73337112f87e482494837e3
Christian Baker <Christian.Baker@echo.com>
2022-03-10 16:38:37 -0600

    Add project files.

---
+++ Data/ApiDbContext.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MusicApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicApi.Data
+{
+    public class ApiDbContext : DbContext
+    {
+        public ApiDbContext(DbContextOptions<ApiDbContext>options) : base(options)
+        {
+
+        }
+
+        public DbSet<Song> Songs { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Song>().HasData(
+                new Song
+                {
+                    Id = 1,
+                    Title = "Willow",
+                    Language = "English",
+                    Duration = "4:35"
+                },
+                new Song
+                {
+                    Id = 2,
+                    Title = "Despacito",
+                    Language = "Spanish",
+                    Duration = "4:15"
+                }
+                );
+        }
+    }
+}
---
+++ Helpers/FileHelper.cs
@@ -0,0 +1,27 @@
+﻿using Azure.Storage.Blobs;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicApi.Helpers
+{
+    public static class FileHelper
+    {
+        public static async Task<string> UploadImage(IFormFile file)
+        {
+            string connectionString = @"DefaultEndpointsProtocol=https;AccountName=udemymusicstorage;AccountKey=CrILnvmA7WLKe77gnDIVTrd0sjdIz/kSYCUsCCTbKFnADBTj90XEiCEDnW1SuDYCoQfvhQfmcjAcBVRF/9vnfA==;EndpointSuffix=core.windows.net";
+            string containerName = "songscover";
+
+            BlobContainerClient blobContainerClient = new BlobContainerClient(connectionString, containerName);
+            BlobClient blobClient = blobContainerClient.GetBlobClient(file.FileName);
+            var memoryStream = new MemoryStream();
+            await file.CopyToAsync(memoryStream);
+            memoryStream.Position = 0;
+            await blobClient.UploadAsync(memoryStream);
+            return blobClient.Uri.AbsoluteUri;
+        }
+    }
+}
